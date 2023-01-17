a628b2dc981a47f212f18dfc74c392744a6080d8
flashback220 <rayhidayat220@gmail.com>
2023-01-17 17:30:59 +0700

    add starter files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Rubik } from "@next/font/google";
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700","800", "900"],
+});
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={rubik.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
