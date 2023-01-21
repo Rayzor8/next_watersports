e461e19cb97595ab6ee4289dd0c77d75f6d793b6
flashback220 <rayhidayat220@gmail.com>
2023-01-22 04:36:28 +0700

    styling contacts and footer components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import navLogo from "../public/images/logo.jpg";
+import Image from "next/image";
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className="container px-5 py-4 mx-auto flex items-center justify-center shadow-lg">
+      <div className="flex gap-2 items-center justify-center">
+        <p className="font-black text-midBlue">
+          @Rayzor{" "}
+          <span className=" text-sm font-normal underline decoration-wavy decoration-sky-400 italic">
+            Watersport
+          </span>
+        </p>
+      </div>
+      <p className="text-sm text-midBlue sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+        © 2022
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- components/MainContent/Contacts.jsx
+++ components/MainContent/Contacts.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import contactImgOne from "../../public/images/contact-1.png";
+import contactImgTwo from "../../public/images/contact-2.png";
+import contactImgThree from "../../public/images/contact-3.png";
+import contactImgFour from "../../public/images/contact-4.png";
 
 const Contacts = () => {
   return (
-    <section className="container mt-12 flex flex-col gap-4 lg:pl-0">
-    <h1>Contacts</h1>
-  </section>
-  )
-}
+    <section className="container mt-12 flex flex-col gap-4 lg:pl-0 justify-center items-center">
+      <h1>Contacts Us</h1>
+      <p>Contacts Information here :</p>
+      <div className="grid  grid-cols-2 md:grid-cols-3 xl:grid-cols-4 w-10/12 2xl:w-1/2 gap-4">
+        <a
+          className="flex flex-col gap-2 bg-white justify-center items-center p-8 rounded shadow-lg cursor-pointer"
+          href="https://www.instagram.com/rayzordev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h2>Instagram</h2>
+          <Image
+            src={contactImgOne}
+            alt="contact-logo"
+            className="w-24 h-24 rounded-full flex-shrink-0 object-cover object-center"
+            priority
+          />
+        </a>
+        <a
+          className="flex flex-col gap-2 bg-white justify-center items-center p-8 rounded shadow-lg cursor-pointer"
+          href="mailto:rayzor8.dev@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h2>Gmail</h2>
+          <Image
+            src={contactImgTwo}
+            alt="contact-logo"
+            className="w-24 h-24 rounded-full flex-shrink-0 object-cover object-center"
+            priority
+          />
+        </a>
+        <a className="flex flex-col gap-2 bg-white justify-center items-center p-8 rounded shadow-lg cursor-pointer">
+          <h2>Whatsapp</h2>
+          <Image
+            src={contactImgThree}
+            alt="contact-logo"
+            className="w-24 h-24 rounded-full flex-shrink-0 object-cover object-center"
+            priority
+          />
+        </a>
+        <a
+          className="flex flex-col gap-2 bg-white justify-center items-center p-8 rounded shadow-lg cursor-pointer"
+          href="https://github.com/Rayzor8"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h2>Github</h2>
+          <Image
+            src={contactImgFour}
+            alt="contact-logo"
+            className="w-24 h-24 rounded-full flex-shrink-0 object-cover object-center"
+            priority
+          />
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Contacts
+export default Contacts;
